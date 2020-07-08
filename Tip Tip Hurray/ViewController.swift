758c13d8df3986c59ec08d3928c06f7ee76f4199
Karen Berba <berbak@oregonstate.edu>
2020-07-07 20:38:02 -0700

    Initial Commit

---
+++ Tip Tip Hurray/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Tip Hurray
+//
+//  Created by Karen Berba on 7/7/20.
+//  Copyright © 2020 Karen Berba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
